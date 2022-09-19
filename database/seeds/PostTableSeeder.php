96699497adfad076bb04964e8a267e48fe190263
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-19 14:27:18 +0200

    create model + faker seed

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PostTableSeeder extends Seeder
 {
@@ -9,8 +11,16 @@ class PostTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 1; $i <= 20; $i++) {
+            $newPost = new Post();
+            $newPost->author = $faker->userName();
+            $newPost->title = $faker->realText(35);
+            $newPost->description = $faker->paragraph(3, true);
+            $newPost->image_url = $faker->imageUrl(350, 350, 'post');
+            $newPost->sale_date = $faker->dateTimeThisYear();
+            $newPost->save();
+        }
     }
 }
