bc4ae754af42260a84a459ac8b13fd202b1e026d
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-19 15:14:45 +0200

    change title on app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'boolpress') }}</title>
+    <title>boolpress</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
